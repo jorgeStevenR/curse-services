8ed795793c2d1704209a6f632729a5f0e3912d42
jorgeStevenR <rodrigesj500@gmail.com>
2026-01-30 20:49:38 -0500

    Initial commit

---
+++ src/main/java/com/devsenior/jorgerodriguez/curseservices/CurseServicesApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.jorgerodriguez.curseservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurseServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurseServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/jorgerodriguez/curseservices/CurseServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.jorgerodriguez.curseservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
